30faece589beb73dbc271c9082b221ed807ed32c
Google Google <Google@students.softuni.bg>
2019-10-01 15:24:03 +0300

    Project Details added

---
+++ MiraDesign.Web/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiraDesign.Web.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
