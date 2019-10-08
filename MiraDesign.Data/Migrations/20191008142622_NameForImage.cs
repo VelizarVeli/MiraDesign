4becba4dc1b7685fc626e30471881759fef848f9
Google Google <Google@students.softuni.bg>
2019-10-08 22:08:00 +0300

    Removed Portfolio from Home page

---
+++ MiraDesign.Data/Migrations/20191008142622_NameForImage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiraDesign.Data.Migrations
+{
+    public partial class NameForImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "Images",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Images",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Images");
+        }
+    }
+}
