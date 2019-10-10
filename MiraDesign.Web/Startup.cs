ca0e16320cd502fcd58aa26e61f6bb1fe4286127
Google Google <Google@students.softuni.bg>
2019-10-10 20:21:53 +0300

    Deployed

--- MiraDesign.Web/Startup.cs
+++ MiraDesign.Web/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,12 +22,10 @@ namespace MiraDesign.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -39,38 +36,15 @@ namespace MiraDesign.Web
 
             services.AddIdentity<MiraDesignUser, IdentityRole>(options =>
                 {
-                    options.Lockout.DefaultLockoutTimeSpan = new TimeSpan(1, 0, 0);
-                    options.Lockout.MaxFailedAccessAttempts = 10;
                 })
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<MiraDesignContext>()
                 .AddDefaultTokenProviders();
 
-            services.Configure<RazorViewEngineOptions>(options =>
-            {
-                options.AreaViewLocationFormats.Clear();
-                options.AreaViewLocationFormats.Add("/Areas/{2}/Views/{1}/{0}.cshtml");
-                options.AreaViewLocationFormats.Add("/Areas/{2}/Views/Shared/{0}.cshtml");
-                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
-            });
-
-            services.AddHttpsRedirection(options =>
-            {
-                options.HttpsPort = 443;
-            });
-
-
             services.AddMvc(options =>
             {
                 options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-                options.Filters.Add(new RequireHttpsAttribute());
             }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-
-
-            //services.AddSingleton<IEmailConfiguration>(Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>());
-            //services.AddTransient<IEmailService, EmailService>();
-            //services.AddScoped<IEmailMessage, EmailMessage>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -84,22 +58,14 @@ namespace MiraDesign.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseAuthentication();
-
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "Administration",
-                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
