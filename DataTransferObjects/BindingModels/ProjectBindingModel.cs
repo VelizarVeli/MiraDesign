ead6228dfa67735d563418c6c72da76bdfeb16a1
Google Google <Google@students.softuni.bg>
2019-11-11 23:53:13 +0200

    ProjectBindingModel

---
+++ DataTransferObjects/BindingModels/ProjectBindingModel.cs
@@ -0,0 +1,11 @@
+﻿using MiraDesign.Models;
+using System.Collections.Generic;
+
+namespace DataTransferObjects.BindingModels
+{
+    public class ProjectBindingModel
+    {
+        public EditProjectBindingModel ProjectModel { get; set; }
+        public ICollection<Image> Images { get; set; }
+    }
+}
